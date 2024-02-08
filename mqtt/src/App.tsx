3c1cd39464f77817e77796c7e4814afee2294d26
leobez <leossbezerra@gmail.com>
2024-02-08 11:54:53 -0300

    modified base project

--- mqtt/src/App.tsx
+++ mqtt/src/App.tsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
-  
-  const [count, setCount] = useState(0)
 
-  return (
-      <>
-      
-      </>
-  )
+    return (
+        <div className="App">
+
+          <Header/>
+
+          <main>
+
+            
+          </main>
+
+          <Footer/>
+
+        </div>
+    )
+
 }
 
 export default App
