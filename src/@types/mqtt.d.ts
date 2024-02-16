60ad4c042336f68cc62d79dd4a60d1c0e394d6db
leobez <leossbezerra@gmail.com>
2024-02-16 18:45:57 -0300

    Added topics and messages to client context

--- src/@types/mqtt.d.ts
+++ src/@types/mqtt.d.ts
@@ -1,6 +1,19 @@
 import { MqttClient } from "mqtt"
 
+export interface Message {
+    topic: string,
+    message: string,
+} 
+
 export type MQTTClientContextType = {
+
     client: MqttClient|null;
-    updateClient: (client:MqttClient|null) => void
+    updateClient: (client:MqttClient|null) => void;
+    
+    topics: string[];
+    updateTopics: (topic:string, action:string) => void;
+
+    messages: Message[];
+    updateMessages: (message:Message) => void;
+
 }
--- src/context/ClientContext.tsx
+++ src/context/ClientContext.tsx
@@ -1,6 +1,6 @@
 import { MqttClient } from "mqtt";
 import { ReactNode, createContext, useState } from "react";
-import { MQTTClientContextType } from "../@types/mqtt";
+import { MQTTClientContextType, Message } from "../@types/mqtt";
 
 export const ClientContext = createContext<MQTTClientContextType|null>(null)
 
@@ -11,14 +11,36 @@ type Props = {
 export const ClientContextProvider = ({children}:Props) => {
 
     const [client, setClient] = useState<MqttClient|null>(null)
+    const [topics, setTopics] = useState<string[]>([])
+    const [messages, setMessages] = useState<Message[]>([])
 
     const updateClient = (client: MqttClient|null):void => {
         setClient(client)
     }
 
+    const updateTopics = (topic:string, action:string):void => {
+        if (action === 'add') {
+            setTopics((prev) => [...prev, topic])
+        }
+        if (action === 'remove') {
+            setTopics((prev) => prev.filter((t) => t !== topic))
+        }
+    }
+
+    const updateMessages = (message:Message):void => {
+        setMessages((prev) => [...prev, message])
+    }
+
     return (
-        <ClientContext.Provider value={{client, updateClient}}>
+        
+        <ClientContext.Provider value={{
+            client, updateClient,
+            topics, updateTopics,
+            messages, updateMessages
+        }}>
+
             {children}
+            
         </ClientContext.Provider>        
     )
 
